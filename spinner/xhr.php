fa4dfa83258680aab9989d75717e940c6b226ba3
gyril <cyril.gantzer@gmail.com>
2011-11-02 23:35:30 +0100

    spinner v0.1

---
+++ spinner/xhr.php
@@ -0,0 +1,24 @@
+<?php
+	$selection = strtolower($_GET['select']);
+	$texte = $_POST['texte'];
+	
+	if(!empty($selection)) {
+		$url = "http://www.crisco.unicaen.fr/des/synonymes/".$selection;
+
+		$file = file_get_contents($url);
+    	header('Content-type: text/xml');
+    	echo $file;
+    	exit();
+    }
+    
+    if(!empty($texte)) {
+    	$body = "content=".$texte."&repetition=10&submit=Générer";
+    	$c = curl_init ("http://www.infowebmaster.fr/outils/content-spinning.php");
+		curl_setopt ($c, CURLOPT_POST, true);
+		curl_setopt ($c, CURLOPT_POSTFIELDS, $body);
+		curl_setopt ($c, CURLOPT_RETURNTRANSFER, true);
+		$page = curl_exec ($c);
+		curl_close ($c);
+		echo utf8_decode(stripslashes($page));
+    }
+?>
